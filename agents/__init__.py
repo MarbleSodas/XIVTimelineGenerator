50a0d7de74397ded03dce1034382077a8c134738
MarbleSodas <eugenepark03@gmail.com>
2026-02-19 13:29:18 -0800

    refactor: remove unused DamageEventExtractorAgent export
    - Remove unused import from agents/__init__.py
    - Class is dead code (never instantiated) after Task 7 consolidation

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,6 +1,5 @@
 from .cactbot_agent import CactbotTimelineAgent
 from .fflogs_agent import FFLogsReportAgent
-from .damage_extractor_agent import DamageEventExtractorAgent
 from .variant_detector_agent import TimelineVariantDetectorAgent
 from .aggregator_agent import TimelineAggregatorAgent
 from .timeline_builder_agent import TimelineBuilderAgent
@@ -9,7 +8,6 @@ from .orchestrator import TimelineGenerationOrchestrator
 __all__ = [
     "CactbotTimelineAgent",
     "FFLogsReportAgent",
-    "DamageEventExtractorAgent",
     "TimelineVariantDetectorAgent",
     "TimelineAggregatorAgent",
     "TimelineBuilderAgent",
